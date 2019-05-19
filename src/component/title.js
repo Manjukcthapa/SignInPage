590f94e3fc0273f716508e785ca9828edb8487de
Manju KC <Manju KC>
2019-05-18 21:54:30 -0400

    add title component

---
+++ src/component/title.js
@@ -0,0 +1,12 @@
+import React from "react"
+ 
+const Titles = () => {
+    return(
+        <div>
+            <h1> Weather Finder</h1>
+            <p>Find out weather, condition and more</p>
+            </div>
+    )
+}
+
+export default Titles
